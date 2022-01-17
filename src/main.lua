f93cb727e9f6d8c4a6813f0fa7933f4bb2da69b0
playsamay4 <70341772+playsamay4@users.noreply.github.com>
2022-01-17 12:35:36 -0800

    Revert "Add color changing code for v1.1.0"
    This reverts commit ba368f7413288240352bfc943616029f49cb1134.

--- src/main.lua
+++ src/main.lua
@@ -4,20 +4,16 @@ function love.load()
     dvdY = love.math.random(-39,360)
     dvdXV = love.math.random(0,1)
     dvdYV = love.math.random(0,1)
-    dvdXSpeed = 176
-    dvdYSpeed = 206
-    dvdXSpeedMax = 176
-    dvdYSpeedMax = 206
-    colorsR = {255,0,0,255}
-    colorsG = {0,255,0,255}
-    colorsB = {0,0,255,0}
-    colorIndex = 1
-    colorIndexLen = 4
+    dvdXSpeed = 100
+    dvdYSpeed = 150
 end
 
 
 function love.update(dt) 
- 
+    print(dvdX)
+    print(dvdY)
+
+
     if dvdXV == 1 then dvdX = dvdX + dvdXSpeed*dt end
     if dvdXV == 0 then dvdX = dvdX - dvdXSpeed*dt end
     if dvdYV == 1 then dvdY = dvdY + dvdYSpeed*dt end
@@ -25,32 +21,25 @@ function love.update(dt)
 
     if dvdX < -15 then 
         dvdXV = 1
-        dvdXSpeed = love.math.random(dvdXSpeedMax,dvdYSpeedMax)
-        colorIndex = colorIndex + 1
+        dvdXSpeed = love.math.random(100,150)
      end
-    if dvdX > (love.graphics.getWidth()*1.01 - dvdSprite:getWidth()) then 
+    if dvdX > (love.graphics.getWidth()*1.03 - dvdSprite:getWidth()) then 
         dvdXV = 0 
-        dvdXSpeed = love.math.random(dvdXSpeedMax,dvdYSpeedMax)
-        colorIndex = colorIndex + 1
+        dvdXSpeed = love.math.random(100,150)
     end
     if dvdY < -40 then 
         dvdYV = 1 
-        dvdYSpeed = love.math.random(dvdXSpeedMax,dvdYSpeedMax)
-        colorIndex = colorIndex + 1
+        dvdYSpeed = love.math.random(100,150)
     end
     if dvdY > (love.graphics.getWidth()/1.5 - dvdSprite:getWidth()) then 
         dvdYV = 0 
-        dvdYSpeed = love.math.random(dvdXSpeedMax,dvdYSpeedMax)
-        colorIndex = colorIndex + 1
+        dvdYSpeed = love.math.random(100,150)
     end
-
-    if colorIndex > colorIndexLen then colorIndex = 1 end
 
 end
 
 
 function love.draw()
-    love.graphics.setColor(colorsR[colorIndex],colorsG[colorIndex],colorsB[colorIndex])
     love.graphics.draw(dvdSprite, dvdX,dvdY,0,1,1)
 end
 
