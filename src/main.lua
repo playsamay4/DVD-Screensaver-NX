fb4f23b7e01d526cab3d8e19f537307a3a596613
playsamay4 <70341772+playsamay4@users.noreply.github.com>
2022-01-29 11:34:09 -0800

    Add color changing code for version 1.1.0

--- src/main.lua
+++ src/main.lua
@@ -1,11 +1,22 @@
 function love.load()
-    dvdSprite = love.graphics.newImage('/assets/dvd.png')
+    dvdSpriteW = love.graphics.newImage('/assets/dvd.png')
+    dvdSpriteR = love.graphics.newImage('/assets/dvdRed.png')
+    dvdSpriteG = love.graphics.newImage('/assets/dvdGreen.png')
+    dvdSpriteB = love.graphics.newImage('/assets/dvdBlue.png')
+    dvdSpriteY = love.graphics.newImage('/assets/dvdYellow.png')
+    dvdSpriteT = love.graphics.newImage('/assets/dvdTeal.png')
+    dvdSpriteP = love.graphics.newImage('/assets/dvdPurple.png')
+    dvdSpriteO = love.graphics.newImage('/assets/dvdOrange.png')
+    dvdSpriteU = dvdSpriteW
     dvdX = love.math.random(-14,1318.4)
     dvdY = love.math.random(-39,360)
     dvdXV = love.math.random(0,1)
     dvdYV = love.math.random(0,1)
     dvdXSpeed = 100
     dvdYSpeed = 150
+    currentSpriteUse = 0
+
+
 end
 
 
@@ -22,25 +33,62 @@ function love.update(dt)
     if dvdX < -15 then 
         dvdXV = 1
         dvdXSpeed = love.math.random(100,150)
+        currentSpriteUse = currentSpriteUse + 1
      end
-    if dvdX > (love.graphics.getWidth()*1.03 - dvdSprite:getWidth()) then 
+    if dvdX > (love.graphics.getWidth()*1.03 - dvdSpriteU:getWidth()) then 
         dvdXV = 0 
         dvdXSpeed = love.math.random(100,150)
+        currentSpriteUse = currentSpriteUse + 1
     end
     if dvdY < -40 then 
         dvdYV = 1 
         dvdYSpeed = love.math.random(100,150)
+        currentSpriteUse = currentSpriteUse + 1
     end
-    if dvdY > (love.graphics.getWidth()/1.5 - dvdSprite:getWidth()) then 
+    if dvdY > (love.graphics.getWidth()/1.5 - dvdSpriteU:getWidth()) then 
         dvdYV = 0 
         dvdYSpeed = love.math.random(100,150)
+        currentSpriteUse = currentSpriteUse + 1
     end
+
+    if currentSpriteUse > 7 then currentSpriteUse = 1 end
 
 end
 
 
 function love.draw()
-    love.graphics.draw(dvdSprite, dvdX,dvdY,0,1,1)
+    if currentSpriteUse == 0 then
+        dvdSpriteU = dvdSpriteW
+        love.graphics.draw(dvdSpriteU, dvdX,dvdY,0,1,1)
+    end
+    if currentSpriteUse == 1 then
+        dvdSpriteU = dvdSpriteR
+        love.graphics.draw(dvdSpriteU, dvdX,dvdY,0,1,1)
+    end
+    if currentSpriteUse == 2 then
+        dvdSpriteU = dvdSpriteG
+        love.graphics.draw(dvdSpriteU, dvdX,dvdY,0,1,1)
+    end
+    if currentSpriteUse == 3 then
+        dvdSpriteU = dvdSpriteB
+        love.graphics.draw(dvdSpriteU, dvdX,dvdY,0,1,1)
+    end
+    if currentSpriteUse == 4 then
+        dvdSpriteU = dvdSpriteY
+        love.graphics.draw(dvdSpriteU, dvdX,dvdY,0,1,1)
+    end
+    if currentSpriteUse == 5 then
+        dvdSpriteU = dvdSpriteT
+        love.graphics.draw(dvdSpriteU, dvdX,dvdY,0,1,1)
+    end
+    if currentSpriteUse == 6 then
+        dvdSpriteU = dvdSpriteP
+        love.graphics.draw(dvdSpriteU, dvdX,dvdY,0,1,1)
+    end
+    if currentSpriteUse == 7 then
+        dvdSpriteU = dvdSpriteO
+        love.graphics.draw(dvdSpriteU, dvdX,dvdY,0,1,1)
+    end
 end
 
 function love.gamepadpressed(joystick,button)
